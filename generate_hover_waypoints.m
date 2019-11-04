4751eba760696a50ba6fac977e79aef9795efffe
michael5511b <37196953+michael5511b@users.noreply.github.com>
2019-11-04 03:36:21 -0500

    State machine implemented.

---
+++ generate_hover_waypoints.m
@@ -0,0 +1,10 @@
+function waypoints_hover = generate_hover_waypoints(theta, hover_z, numOfPoints)
+    
+    waypoints_x = zero(1, numOfPoints);
+    waypoints_y = zero(1, numOfPoints);
+    waypoints_z = hover_z * ones(1, numOfPoints);
+    waypoints_theta = theta * ones(1, numOfPoints);
+    
+    waypoints_hover = [waypoints_x, waypoints_y, waypoints_z, waypoints_theta];
+    
+end
